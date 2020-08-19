29fbaa53d7731ace5fe3d9e80bf9a53620bbe7ff
Ganesh Maurya <gvgmaurya@gmail.com>
2020-08-19 21:16:54 +0530

    Linked List in C - Inserting a node at beginning

--- C/Linked_List_Inserting.c
+++ C/Linked_List_Inserting.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <conio.h>
 
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+struct node* head = NULL;
+
+void Insert(int data)
+{
+    struct node* new;
+    new = (struct node *)malloc(sizeof(struct node));
+    new->data = data;
+    new->next = head;
+    head = new;
+}
+
+void Print()
+{
+    struct node* temp = head;
+    while(temp != NULL)
+    {
+        printf("List is : ");
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int n;
+    printf("How many numbers : ");
+    scanf("%d", &n);
+
+    for (int i=0; i<n ; i++)
+    {
+        int x;
+        printf("Enter number to be inserted : ");
+        scanf("%d", &x);
+        Insert(x);
+        Print();
+    }
+
+    return 0;
+}
