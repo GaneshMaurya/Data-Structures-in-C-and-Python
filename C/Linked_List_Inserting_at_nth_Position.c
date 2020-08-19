ea64770de12459631197d5c1b66ac8344e5fa748
Ganesh Maurya <gvgmaurya@gmail.com>
2020-08-19 21:43:21 +0530

    Linked List in C - Insert a node at nth position

---
+++ C/Linked_List_Inserting_at_nth_Position.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+struct node* head = NULL;
+
+void Print()
+{
+    struct node* temp = head;
+    printf("List is : \n");
+    while(temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+void Insert(int data, int position)
+{
+    struct node * new = (struct node *)malloc(sizeof(struct node));
+    if (position == 1) {
+        new->data = data;
+        new->next = head;
+        head = new;
+        return;
+    }
+    else {
+        new->data = data;
+        struct node * temp = head;
+        for (int i=0; i<position-2; i++) {
+            temp = temp->next;
+        }
+        new->next = temp->next;
+        temp->next = new;
+    }
+}
+
+int main() 
+{
+    Insert(3,1);
+    Insert(2,2);
+    Insert(1,3);
+    Insert(4,2);
+    Print();
+    
+    return 0;
+}
