04845c508c74d1a1deb4b3608b0ca05f06808ba4
Ganesh Maurya <gvgmaurya@gmail.com>
2021-06-26 23:48:50 +0530

    arrays day-2 (vii)

---
+++ C/find_duplicates_in_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {2, 7, 1, 4, 1, 7, 8, 2, 8};
+    int n = sizeof(arr)/sizeof(int);
+
+    for (int i=0; i<n; i++)
+    {
+        arr[arr[i]%n] += n;
+    }
+
+    for (int i=0; i<n; i++)
+    {
+        if (arr[i]/n > 1)
+        {
+            printf("%d ", arr[i]%n);
+        }
+    }
+
+    return 0;
+}
