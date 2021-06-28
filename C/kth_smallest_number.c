edd29139b5834dacc0c73461e4760986d69e7d68
Ganesh Maurya <gvgmaurya@gmail.com>
2021-06-28 14:05:01 +0530

    arrays day-3 (i)

--- C/kth_smallest_number.c
+++ C/kth_smallest_number.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 void swap(int *a, int *b)
 {
@@ -12,22 +11,22 @@ int partition(int arr[], int s, int e)
 {
     int pivot = arr[e];
     int pi = s;
-    
+
     for (int i=s; i<e-1; i++)
     {
-        if (arr[i] <= pivot)
+        if (arr[i] >= pivot)
         {
             swap(&arr[i], &arr[pi]);
             pi++;
         }
     }
     swap(&arr[pi], &arr[e]);
-    return pi+1;
+    return (pi+1);
 }
 
 void quicksort(int arr[], int s, int e)
 {
-    if (s >= e)
+    if (s>=e)
     {
         return;
     }
@@ -38,34 +37,13 @@ void quicksort(int arr[], int s, int e)
 
 int main()
 {
-    int arr[] = {11, 15, 6, 8, 9, 10};
-    int sum = 16;
+    int arr[] = {7, 10, 4, 3, 20, 15};
+    int k = 3;
     int n = sizeof(arr)/sizeof(int);
 
     quicksort(arr, 0, n-1);
 
-    int l = 0;
-    int r = n-1;
-    bool res = false;
-
-    while (l != r)
-    {
-        if (arr[l] + arr[r] == sum)
-        {
-            res = true;
-            break;
-        }
-        else if (arr[l] + arr[r] > sum)
-        {
-            r--;
-        }
-        else
-        {
-            l++;
-        }
-    }
-
-    printf(res ? "Yes. There exists a pair." : "No there doesn't exist any pair.");
+    printf("%d", arr[k-1]);
     
     return 0;
 }
