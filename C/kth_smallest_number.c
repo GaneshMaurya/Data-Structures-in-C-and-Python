d109209714f720e0bea6b804ce67fb96a7040951
Ganesh Maurya <gvgmaurya@gmail.com>
2021-06-28 14:06:05 +0530

    modified

--- C/kth_smallest_number.c
+++ C/kth_smallest_number.c
@@ -1,3 +1,7 @@
+/*Given an array and a number k where k is smaller than the size of the array, 
+we need to find the k’th smallest element in the given array. 
+It is given that all array elements are distinct.*/
+
 #include <stdio.h>
 
 void swap(int *a, int *b)
