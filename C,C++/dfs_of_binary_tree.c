1921b9e65c38977c851301e5df07317800e7003a
Ganesh Maurya <gvgmaurya@gmail.com>
2021-07-08 19:59:15 +0530

    dfs of binary tree

---
+++ C,C++/dfs_of_binary_tree.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node * left;
+    struct node * right;
+};
+
+struct node * newNode(int data)
+{
+    struct node * temp = (struct node *)malloc(sizeof(struct node));
+
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+void inorder(struct node * root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+void preorder(struct node * root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(struct node * root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+
+int main()
+{
+    struct node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    inorder(root);
+    printf("\n");
+    preorder(root);
+    printf("\n");
+    postorder(root);
+    printf("\n");
+    
+    return 0;
+}
