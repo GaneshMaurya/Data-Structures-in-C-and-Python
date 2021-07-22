bf651fe065d992cbf5a9b4829fd5138807485182
Ganesh Maurya <gvgmaurya@gmail.com>
2021-07-22 21:49:55 +0530

    small changes

--- C,C++/largest_sum_subtree.cpp
+++ C,C++/largest_sum_subtree.cpp
@@ -2,6 +2,8 @@
 #include <limits.h>
 using namespace std;
 
+// int maxi = INT_MIN;
+
 struct node 
 {
     int data;
@@ -56,16 +58,29 @@ int getMaxSumSubtree(struct node * root)
     return ans;
 }
 
+// int getMaxSumSubtree(struct node * root)
+// {
+//     if (root == NULL)
+//     {
+//         return 0;
+//     }
+
+//     int ans = root->data + getMaxSumSubtree(root->left) + getMaxSumSubtree(root->right);
+//     maxi = maximum(maxi, ans);
+
+//     return ans;
+// }
+
 int main()
 {
     struct node * root = (struct node *)malloc(sizeof(struct node));
     root = addNode(1);
-    root->left = addNode(2);
+    root->left = addNode(-2);
     root->right = addNode(3);
     root->left->left = addNode(4);
     root->left->right = addNode(5);
-    root->right->left = addNode(6);
-    root->right->right = addNode(7);
+    root->right->left = addNode(-6);
+    root->right->right = addNode(2000);
 
     cout<<getMaxSumSubtree(root);
     
