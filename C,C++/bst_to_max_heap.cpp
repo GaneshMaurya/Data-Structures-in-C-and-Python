0bfe8f3b722aa7f48887a200d15831f0c5a250a5
Ganesh Maurya <gvgmaurya@gmail.com>
2021-07-30 16:57:53 +0530

    same

--- C,C++/bst_to_max_heap.cpp
+++ C,C++/bst_to_max_heap.cpp
@@ -1,10 +1,3 @@
-/*
-Given a binary search tree which is also a complete binary tree. 
-The problem is to convert the given BST into a Min Heap with the condition that all the 
-values in the left subtree of a node should be less than all the values in the right subtree of the node. 
-This condition is applied on all the nodes in the so converted Min Heap. 
-*/
-
 #include <bits/stdc++.h>
 using namespace std;
 
