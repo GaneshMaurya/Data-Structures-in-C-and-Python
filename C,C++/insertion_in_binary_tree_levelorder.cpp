30f8010f1eee4f05565e7e4ca5ffc0fff818afdb
Ganesh Maurya <gvgmaurya@gmail.com>
2021-08-27 15:53:03 +0530

    insertion in binary tree levelorder

---
+++ C,C++/insertion_in_binary_tree_levelorder.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node 
+{
+    int data;
+    struct node * left;
+    struct node * right;
+};
+
+struct node * newNode(int data)
+{
+    struct node * temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+struct node * insertNode(struct node * root, int data)
+{
+    if (root == NULL)
+    {
+        return newNode(data);
+    }
+    else
+    {
+        queue<struct node *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            struct node * temp = q.front();
+            q.pop();
+
+            if (temp->left != NULL)
+            {
+                q.push(temp->left);
+            }
+            else
+            {
+                temp->left = newNode(data);
+                return root;
+            }
+
+            if (temp->right != NULL)
+            {
+                q.push(temp->right);
+            }
+            else
+            {
+                temp->right = newNode(data);
+                return root;
+            }
+        }
+
+        return root;
+    }
+}
+
+void inorder(struct node * root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    else
+    {
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+
+        return;
+    }
+}
+
+void levelorder(struct node * root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    else 
+    {
+        queue<struct node *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            struct node * temp = q.front();
+            cout<<temp->data<<" ";
+            q.pop();
+
+            if (temp->left != NULL)
+            {
+                q.push(temp->left);
+            }
+
+            if (temp->right != NULL)
+            {
+                q.push(temp->right);
+            }
+        }
+
+        return;
+    }
+}
+
+
+int main()
+{
+    struct node * root = NULL;
+    root = insertNode(root, 10);
+    root = insertNode(root, 20);
+    root = insertNode(root, 30);
+    root = insertNode(root, 40);
+    root = insertNode(root, 50);
+    root = insertNode(root, 60);
+
+    inorder(root);
+    cout<<"\n";
+    levelorder(root);
+
+    return 0;
+}
