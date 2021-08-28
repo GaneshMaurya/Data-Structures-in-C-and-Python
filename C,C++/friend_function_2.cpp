1575fd00cb6080cc90dc9d9843b846b0127cbcad
Ganesh Maurya <gvgmaurya@gmail.com>
2021-08-29 00:26:40 +0530

    same

--- C,C++/friend_function_2.cpp
+++ C,C++/friend_function_2.cpp
@@ -6,39 +6,41 @@ class Y;
 class X
 {
     int data;
-    public:
-        void getData(int n)
-        {
-            data = n;
-        }
-        friend void Sum(X, Y);
+
+public:
+    void getData(int n)
+    {
+        data = n;
+    }
+    friend void Sum(X, Y);
 };
 
 class Y
 {
     int data;
-    public:
-        void getData(int n)
-        {
-            data = n;
-        }
-        friend void Sum(X, Y);
+
+public:
+    void getData(int n)
+    {
+        data = n;
+    }
+    friend void Sum(X, Y);
 };
 
 void Sum(X o1, Y o2)
 {
-    cout<<o1.data+o2.data;
+    cout << o1.data + o2.data;
 }
 
 int main()
 {
     X a;
     a.getData(10);
-    
+
     Y b;
     b.getData(20);
 
     Sum(a, b);
-    
+
     return 0;
 }
