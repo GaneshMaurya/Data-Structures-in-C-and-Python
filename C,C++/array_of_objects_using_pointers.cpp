1800313bfdfd6f18110a94d953234ee0d1272aca
Ganesh Maurya <gvgmaurya@gmail.com>
2021-09-12 13:07:16 +0530

    same

--- C,C++/array_of_objects_using_pointers.cpp
+++ C,C++/array_of_objects_using_pointers.cpp
@@ -5,23 +5,24 @@ class Shop
 {
     int id;
     float price;
-    public:
-        void setData(int a, float b)
-        {
-            id = a;
-            price = b;
-        }
-        void printData()
-        {
-            cout<<"ID : "<<id<<"\n";
-            cout<<"Price : "<<price<<"\n";
-        }
+
+public:
+    void setData(int a, float b)
+    {
+        id = a;
+        price = b;
+    }
+    void printData()
+    {
+        cout << "ID : " << id << "\n";
+        cout << "Price : " << price << "\n";
+    }
 };
 
 int main()
 {
     int size = 3;
-    Shop * ptr = new Shop[size];
+    Shop *ptr = new Shop[size];
     // ptr->setData(5, 23.0);
     // ptr->printData();
 
@@ -33,20 +34,20 @@ int main()
 
     // *(ptr+3).setData(8, 100);
     // *(ptr+3).printData();
-    
-    Shop * temp = ptr;
-    for (int i=0; i<size; i++)
+
+    Shop *temp = ptr;
+    for (int i = 0; i < size; i++)
     {
         int a;
         float b;
-        cout<<"Enter ID and Price of Item-"<<i+1<<": \n";
-        cin>>a>>b;
+        cout << "Enter ID and Price of Item-" << i + 1 << ": \n";
+        cin >> a >> b;
         temp->setData(a, b);
         temp++;
     }
 
     temp = ptr;
-    for (int i=0; i<size; i++)
+    for (int i = 0; i < size; i++)
     {
         temp->printData();
         temp++;
