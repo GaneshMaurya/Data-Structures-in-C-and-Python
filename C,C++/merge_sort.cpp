4a28e83dfc690ad48545305703144d915e13fde3
Ganesh Maurya <gvgmaurya@gmail.com>
2022-01-07 22:51:35 +0530

    same

--- C,C++/merge_sort.cpp
+++ C,C++/merge_sort.cpp
@@ -15,7 +15,7 @@ void merge(int arr[], int low, int mid, int high)
     int i = low;
     int j = mid+1;
     int k = low;
-    int B[high+1];
+    int B[1001];
     while (i <= mid && j <= high)
     {
         if (arr[i] < arr[j])
